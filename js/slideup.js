857975bace948c51494701927a854fd55ae67bfd
antwort7 <antwort7@gmail.com>
2014-05-24 18:25:27 -0500

    finally able to include actual files

---
+++ js/slideup.js
@@ -0,0 +1,12 @@
+<script>
+	$(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+</script>
